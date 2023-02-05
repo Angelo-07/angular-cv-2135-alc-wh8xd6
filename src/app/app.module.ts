b304faf015f6b697be7e3b9c9324493cacc77872
Angelo-07 <98503954+Angelo-07@users.noreply.github.com>
2023-02-05 13:57:25 +0800

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,27 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SkillsComponent } from './skills/skills.component';
 import { AboutComponent } from './about/about.component';
 import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
 import { ReferencesComponent } from './references/references.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, AboutComponent, EducationComponent, SkillsComponent, ReferencesComponent ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    AchievementsComponent,
+    ReferencesComponent,
+    PagenotfoundComponent
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
